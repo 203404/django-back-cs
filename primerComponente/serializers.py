14ce8322cce205aaef38dadf23a4fa9e5af069ef
Ale Mota <203404@ids.upchiapas.edu.mx>
2022-01-22 19:18:23 -0600

    Login component

--- primerComponente/serializers.py
+++ primerComponente/serializers.py
@@ -1,1 +1,8 @@
 from rest_framework import routers, serializers, viewsets
+
+from primerComponente.models import PrimerTabla
+
+class PrimerTablaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PrimerTabla
+        fields = ( 'nombre', 'edad')
--- primerComponente/views.py
+++ primerComponente/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+#Importaciones de models
+from primerComponente.models import PrimerTabla
+
+#importaciones de serializers
+from primerComponente.serializers import PrimerTablaSerializer
 
 # Create your views here.
+class PrimerTablaList(APIView):
+    def get(self, request, format=None):
+        queryset = PrimerTabla.objects.all()
+        serializer = PrimerTablaSerializer(queryset, many=True, context=({"request": request}))
+        return Response(serializer.data)
